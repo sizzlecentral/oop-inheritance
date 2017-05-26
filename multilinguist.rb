b8c167cbbb05410c1a715dc98378abb9509e7b28
Don Cooper <don_cooper@hotmail.com>
2017-05-26 14:16:28 -0400

    Quote collector done

--- multilinguist.rb
+++ multilinguist.rb
@@ -52,4 +52,3 @@ class Multilinguist
     json_response['translationText']
   end
 end
-
--- quote-collector.rb
+++ quote-collector.rb
@@ -14,9 +14,11 @@ class QuoteCollector < Multilinguist
   end
 
   def unleash
+    translated = []
     @@quotes.each do |quote|
-      say_in_local_language(quote) # returning quotes, but not in correct language
+      translated << say_in_local_language(quote)
     end
+    return translated
   end
 
   def random_quote
@@ -34,6 +36,6 @@ me.add_new_quote("I think, therefore I am.")
 me.add_new_quote("To be, or not to be, that is the question.")
 me.travel_to("France")
 puts me.unleash
-me.travel_to("Italy")
-puts me.unleash
+# me.travel_to("Italy")
+# puts me.unleash
 # puts me.random_quote
