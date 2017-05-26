68852565f537b1d9b8ec3042403d8659122489a3
Don Cooper <don_cooper@hotmail.com>
2017-05-26 07:59:34 -0400

    Add New Quote and Random Quote working

---
+++ quote-collector.rb
@@ -0,0 +1,37 @@
+# QuoteCollector
+# --------------
+
+require_relative 'multilinguist'
+require 'httparty'
+require 'json'
+
+class QuoteCollector < Multilinguist
+
+  @@quotes = []
+
+  def add_new_quote(string)
+    @@quotes << string
+  end
+
+  def unleash
+    @@quotes.each do |quote|
+      say_in_local_language(quote)
+    end
+  end
+
+  def random_quote
+    msg = @@quotes.sample
+    say_in_local_language(msg)
+  end
+
+end
+
+# ----- End of QuoteCollector class ----- #
+
+me = QuoteCollector.new
+
+me.add_new_quote("I think, therefore I am.")
+me.add_new_quote("To be, or not to be, that is the question.")
+me.travel_to("France")
+# puts me.unleash
+puts me.random_quote
