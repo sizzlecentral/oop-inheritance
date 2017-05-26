6573f67619b5a1b0d634e783bde75b0c600725ca
Don Cooper <don_cooper@hotmail.com>
2017-05-26 13:36:08 -0400

    Add and Total Mass methods created

--- solar-system.rb
+++ solar-system.rb
@@ -5,20 +5,44 @@ class System
 
   attr_reader :bodies
 
-  @@bodies - []
+  def initialize
+    @@bodies = []
+  end
 
 # ----- Class Methods ----- #
 
+  def self.all
+    @@bodies
+  end
+
+  def self.create
+    System.new
+  end
 
 
 
 # ----- Instance Methods ----- #
 
-  def add
-    
+  def add(planet)
+    @@bodies << planet
+  end
+
+  def total_mass
+    mass = []
+    @@bodies.each do |planet|
+      mass << planet.mass
+    end
+    return mass.sum
   end
 
 
 end
 
 # ----- End of System class ----- #
+
+milkyway = System.create
+
+# p System.all
+milkyway.add("Neptune")
+milkyway.add("Venus")
+p System.all
