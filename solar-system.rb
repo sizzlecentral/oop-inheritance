423314688f65a28de7de1016fc5c2c8d8d924b08
Don Cooper <don_cooper@hotmail.com>
2017-05-26 13:11:43 -0400

    Starting Solar System exercise

---
+++ solar-system.rb
@@ -0,0 +1,24 @@
+# Solar System
+# ------------
+
+class System
+
+  attr_reader :bodies
+
+  @@bodies - []
+
+# ----- Class Methods ----- #
+
+
+
+
+# ----- Instance Methods ----- #
+
+  def add
+    
+  end
+
+
+end
+
+# ----- End of System class ----- #
